605d607e3db50c271edccd42d605d0a2d61d0c36
Guðlaugur S. Egilsson <gulli@kolibri.is>
2019-10-19 16:03:54 +0000

    Update types

--- packages/typescript-types/src/index.ts
+++ packages/typescript-types/src/index.ts
@@ -1,3 +1,4 @@
+
 export type TDeploymentState = {
     new: boolean
     key: string
@@ -22,5 +23,50 @@ export interface IStorageBackend {
 export type TStateStoreDependencies = {
     storageBackend: IStorageBackend
 }
+
+
+export type THerdSpec ={
+    key: string // Key used in herd file
+    image: string // For example isrvkbuild02:5000/fluentd
+    imagetag: string // For example v1.1.2-g-2b48d1c
+    description: string //"Log writer to AWS ES/Kibana"
+}
+
+export enum TDeploymentType {
+    Deployer,
+    Kubernetes,
+    Infrastructure,
+    Migration
+}
+
+export type TShepherdMetadata = {
+    displayName: string
+    deploymentType: TDeploymentType
+    migrationImage?: string
+    lastCommits: string
+    gitUrl: string
+    gitHash: string
+    buildDate: Date
+    gitCommit?: string
+    dockerImageTag?: string
+    buildHostName?: string
+    hyperlinks?: Array<THref>
+}
+
+
+export type TShepherdExtendedMetadata = TShepherdMetadata & {
+    id: string
+    herdSpec: THerdSpec
+    deploymentState: TDeploymentState
+    timestamp?: Date;
+}
+
+
+type THref = {
+    title: string
+    url: string
+}
+
+
 
 
