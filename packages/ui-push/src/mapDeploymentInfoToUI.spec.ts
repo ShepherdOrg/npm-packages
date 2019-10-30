a4f7d2a3ea0c310c723433cb9f529eb24feae7e7
Einar Nordfjord <einar@nordfjord.io>
2019-10-30 16:10:31 +0000

    ui-push package

--- packages/ui-push/src/mapDeploymentInfoToUI.spec.ts
+++ packages/ui-push/src/mapDeploymentInfoToUI.spec.ts
@@ -1,125 +1,37 @@
-import {
-  CreateDeploymentInput,
-  CreateDeploymentVersionInput,
-  DeployerRole,
-  DeploymentType,
-} from "@shepherdorg/ui-graphql-client/dist/src/API"
-import { THerdDeployerMetadata } from "./temptypes"
 import { expect } from "chai"
 import { getValidHerdDeployerMetadata } from "./testdata"
-import { TDeployerRole, TDeploymentType } from "@shepherdorg/metadata/dist"
-
-export type DeploymentUIInfo = {
-  versionInfo: CreateDeploymentVersionInput
-  deploymentInfo: CreateDeploymentInput
-}
-
-function mapDeploymentType(deploymentType: TDeploymentType) {
-  const mapping: { [K in TDeploymentType]: DeploymentType } = {
-    [TDeploymentType.Deployer]: DeploymentType.Deployer,
-    [TDeploymentType.Kubernetes]: DeploymentType.Kubernetes,
-  }
-  return mapping[deploymentType]
-}
-
-function mapDeployerRole(deployerRole: TDeployerRole) {
-  const mapping: { [K in TDeployerRole]: DeployerRole } = {
-    [TDeployerRole.Infrastructure]: DeployerRole.Infrastructure,
-    [TDeployerRole.Install]: DeployerRole.Install,
-    [TDeployerRole.Migration]: DeployerRole.Migration,
-  }
-  return mapping[deployerRole]
-}
+import { mapToUiVersion } from "./mapDeploymentInfoToUI"
+import { DeploymentVersion, Deployment } from "@shepherdorg/ui-graphql-client"
 
 describe("mapping", function() {
-  function mapToUiVersion(
-    deployerInfo: THerdDeployerMetadata
-  ): DeploymentUIInfo {
-    function mapLinks() {
-      if (deployerInfo.hyperlinks) {
-        return deployerInfo.hyperlinks.map(link => {
-          return { title: link.title, url: link.url }
-        })
-      } else {
-        return []
-      }
-    }
-
-    if (deployerInfo.deploymentState.timestamp) {
-      let deployedAt = deployerInfo.deploymentState.timestamp.toISOString()
-      return {
-        versionInfo: {
-          buildHostName: deployerInfo.buildHostName || "build host is unknown",
-          builtAt: deployerInfo.buildDate.toISOString(),
-          deployedAt: deployedAt,
-          deploymentVersionDeploymentId:
-            deployerInfo.deploymentState.env + deployerInfo.herdSpec.key,
-          dockerImage: deployerInfo.dockerImageTag || "missing docker image", // TODO Must fix in metadata library
-          dockerImageTag:
-            deployerInfo.dockerImageTag || "missing docker image tag",
-          env: deployerInfo.deploymentState.env,
-          gitBranch: deployerInfo.deploymentState.env,
-          gitCommit: deployerInfo.gitCommit || "missing",
-          gitHash: deployerInfo.gitHash,
-          gitUrl: deployerInfo.gitUrl,
-          kubernetesDeploymentFiles: [],
-          lastCommits: deployerInfo.lastCommits,
-          version: deployerInfo.semanticVersion,
-          versionId:
-            deployerInfo.deploymentState.env +
-            deployerInfo.herdSpec.key +
-            deployerInfo.deploymentState.version +
-            deployedAt,
-        },
-        deploymentInfo: {
-          id: deployerInfo.deploymentState.env + deployerInfo.herdSpec.key,
-          displayName:
-            deployerInfo.herdSpec.description || deployerInfo.displayName,
-          description: deployerInfo.herdSpec.description,
-          deploymentType: mapDeploymentType(deployerInfo.deploymentType),
-          deployerRole: mapDeployerRole(deployerInfo.deployerRole),
-          dbMigrationImage: deployerInfo.migrationImage,
-          hyperlinks: mapLinks(),
-          lastDeploymentTimestamp: deployedAt,
-          env: deployerInfo.deploymentState.env,
-        },
-      }
-    } else {
-      throw new Error(
-        "Deployment lacks sufficient information for pushing to UI!"
-      )
-    }
-  }
-
   it("should map deployer info to UI", () => {
     const input = getValidHerdDeployerMetadata()
 
-    const expectedVersionInfo = {
-      buildHostName: "Gulaugurs-MacBook-Pro.local",
-      builtAt: "2019-10-21T14:53:18.000Z",
-      deployedAt: "2019-10-21T14:53:18.000Z",
-      deploymentVersionDeploymentId: "devdev-images-plain-deployer",
-      dockerImage: "plain-deployer-repo:latest",
-      dockerImageTag: "plain-deployer-repo:latest",
+    const expectedVersionInfo: DeploymentVersion = {
+      build_host_name: "Gulaugurs-MacBook-Pro.local",
+      built_at: "2019-10-21T14:53:18.000Z",
+      deployed_at: "2019-10-21T14:53:18.000Z",
+      deployment_id: "devdev-images-plain-deployer",
+      docker_image: "plain-deployer-repo:latest",
+      docker_image_tag: "plain-deployer-repo:latest",
       env: "dev",
-      gitBranch: "dev",
-      gitCommit: "2153e378877c0deaa2a3ee2491800d40f5212bc5",
-      gitHash: "062b591",
-      gitUrl: "git@github.com:ShepherdOrg/npm-packages.git",
-      kubernetesDeploymentFiles: [],
-      lastCommits:
+      git_branch: "dev",
+      git_commit: "2153e378877c0deaa2a3ee2491800d40f5212bc5",
+      git_hash: "062b591",
+      git_url: "git@github.com:ShepherdOrg/npm-packages.git",
+      kubernetes_deployment_files: [],
+      last_commits:
         " Sun, 20 Oct 2019 17:55:38 +0000 by Guðlaugur S. Egilsson. --- Metadata updates and a few more fixes. \n\n Wed, 9 Oct 2019 13:52:56 +0000 by Guðlaugur S. Egilsson. --- First pass on shepherd-inspect, not ready at all though. \n\n Mon, 7 Oct 2019 14:28:50 +0000 by Guðlaugur S. Egilsson. --- Adding json schema for validating shepherd.json config file \n\n Fri, 4 Oct 2019 15:00:24 +0000 by Guðlaugur S. Egilsson. --- Introducing npm installable build docker script. Changing docker label for metadata packaging \n",
       version: "latest",
-      versionId:
-        "devdev-images-plain-deployer999.999.999992019-10-21T14:53:18.000Z",
+      id: "devdev-images-plain-deployer999.999.999992019-10-21T14:53:18.000Z",
     }
 
-    let expectedDeploymentInfo = {
+    let expectedDeploymentInfo: Deployment = {
       id: "devdev-images-plain-deployer",
-      displayName: "Log writer to AWS ES/Kibana",
+      display_name: "Log writer to AWS ES/Kibana",
       description: "Log writer to AWS ES/Kibana",
-      deploymentType: "Deployer",
-      deployerRole: "Install",
+      deployment_type: "Deployer",
+      deployer_role: "Install",
       hyperlinks: [
         { title: "TestlinkOne", url: "https://link.to.nowhere" },
         {
@@ -127,15 +39,13 @@ describe("mapping", function() {
           url: "https://link.to.elsewhere",
         },
       ],
-      dbMigrationImage: "plain-testing-migrationimage",
-      lastDeploymentTimestamp: "2019-10-21T14:53:18.000Z",
+      db_migration_image: "plain-testing-migrationimage",
+      last_deployment_timestamp: "2019-10-21T14:53:18.000Z",
       env: "dev",
     }
 
     let deploymentUIInfo = mapToUiVersion(input)
     expect(deploymentUIInfo.versionInfo).to.deep.equal(expectedVersionInfo)
-    expect(deploymentUIInfo.deploymentInfo).to.deep.equal(
-      expectedDeploymentInfo
-    )
+    expect(deploymentUIInfo.deploymentInfo).to.deep.equal(expectedDeploymentInfo)
   })
 })
---
+++ packages/ui-push/src/mapDeploymentInfoToUI.ts
@@ -0,0 +1,80 @@
+import { THerdDeployerMetadata } from "./temptypes"
+import { TDeployerRole, TDeploymentType } from "@shepherdorg/metadata"
+import { DeploymentVersion, Deployment } from "@shepherdorg/ui-graphql-client"
+
+export interface DeploymentUIInfo {
+  versionInfo: DeploymentVersion
+  deploymentInfo: Deployment
+}
+
+function mapDeploymentType(deploymentType: TDeploymentType): string {
+  switch (deploymentType) {
+    case TDeploymentType.Kubernetes:
+      return "Kubernetes"
+    case TDeploymentType.Deployer:
+      return "Deployer"
+  }
+}
+
+function mapDeployerRole(deployerRole: TDeployerRole): string {
+  switch (deployerRole) {
+    case TDeployerRole.Infrastructure:
+      return "Infrastructure"
+    case TDeployerRole.Install:
+      return "Install"
+    case TDeployerRole.Migration:
+      return "Migration"
+  }
+}
+
+export function mapToUiVersion(deployerInfo: THerdDeployerMetadata): DeploymentUIInfo {
+  function mapLinks() {
+    if (deployerInfo.hyperlinks) {
+      return deployerInfo.hyperlinks.map(link => {
+        return { title: link.title, url: link.url }
+      })
+    } else {
+      return []
+    }
+  }
+
+  if (deployerInfo.deploymentState.timestamp) {
+    let deployedAt = deployerInfo.deploymentState.timestamp.toISOString()
+    return {
+      versionInfo: {
+        build_host_name: deployerInfo.buildHostName || "build host is unknown",
+        built_at: deployerInfo.buildDate.toISOString(),
+        deployed_at: deployedAt,
+        deployment_id: deployerInfo.deploymentState.env + deployerInfo.herdSpec.key,
+        docker_image: deployerInfo.dockerImageTag || "missing docker image", // TODO Must fix in metadata library
+        docker_image_tag: deployerInfo.dockerImageTag || "missing docker image tag",
+        env: deployerInfo.deploymentState.env,
+        git_branch: deployerInfo.deploymentState.env,
+        git_commit: deployerInfo.gitCommit || "missing",
+        git_hash: deployerInfo.gitHash,
+        git_url: deployerInfo.gitUrl,
+        kubernetes_deployment_files: [],
+        last_commits: deployerInfo.lastCommits,
+        version: deployerInfo.semanticVersion,
+        id:
+          deployerInfo.deploymentState.env +
+          deployerInfo.herdSpec.key +
+          deployerInfo.deploymentState.version +
+          deployedAt,
+      },
+      deploymentInfo: {
+        id: deployerInfo.deploymentState.env + deployerInfo.herdSpec.key,
+        display_name: deployerInfo.herdSpec.description || deployerInfo.displayName,
+        description: deployerInfo.herdSpec.description,
+        deployment_type: mapDeploymentType(deployerInfo.deploymentType),
+        deployer_role: mapDeployerRole(deployerInfo.deployerRole),
+        db_migration_image: deployerInfo.migrationImage,
+        hyperlinks: mapLinks(),
+        last_deployment_timestamp: deployedAt,
+        env: deployerInfo.deploymentState.env,
+      },
+    }
+  } else {
+    throw new Error("Deployment lacks sufficient information for pushing to UI!")
+  }
+}
