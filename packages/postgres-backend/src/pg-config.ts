167f86a41288c704074ccd77dce1da2111386918
Ólafur Torfi Yngvason <olafur.yngvason@kvika.is>
2025-02-18 14:53:05 +0000

    r1434: rename shepherd pg root ssl crt variable

--- packages/postgres-backend/src/pg-config.ts
+++ packages/postgres-backend/src/pg-config.ts
@@ -17,7 +17,9 @@ export function PgConfig(): PGConnectionConfig {
     password: process.env.SHEPHERD_PG_PASSWORD || "mysecretpassword",
     port: Number(process.env.SHEPHERD_PG_PORT) || 5432,
     idleTimeoutMillis: 30000,
-    ssl: process.env.PG_ROOT_SSL_CRT ? { rejectUnauthorized: false, crt: process.env.PG_ROOT_SSL_CRT } : undefined,
+    ssl: process.env.SHEPHERD_PG_ROOT_SSL_CRT
+      ? { rejectUnauthorized: false, crt: process.env.SHEPHERD_PG_ROOT_SSL_CRT }
+      : undefined,
     schema: process.env.SHEPHERD_PG_SCHEMA || "shepherd",
   }
 }
